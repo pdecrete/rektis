9626089e981b16cd44fb6cdd9954f9c96d894a58
Manos Poulakis <manpoul@gmail.com>
2016-09-28 13:22:11 +0300

    Specialisation actions Translation

--- yii2/views/specialisation/create.php
+++ yii2/views/specialisation/create.php
@@ -6,8 +6,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Specialisation */
 
-$this->title = 'Create Specialisation';
-$this->params['breadcrumbs'][] = ['label' => 'Specialisations', 'url' => ['index']];
+$this->title = Yii::t('app', 'Create Specialisation');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Specialisations'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="specialisation-create">
--- yii2/views/specialisation/index.php
+++ yii2/views/specialisation/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $searchModel app\models\SpecialisationSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Specialisations';
+$this->title = Yii::t('app', 'Specialisations');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="specialisation-index">
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Specialisation', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Yii::t('app', 'Create Specialisation'), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
