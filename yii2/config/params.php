8b550a7ae6023c369ad988d63091afeb711b95ac
Manos Poulakis <manpoul@gmail.com>
2016-10-26 11:32:20 +0300

    New parameters in param.php

--- yii2/config/params.php
+++ yii2/config/params.php
@@ -6,4 +6,12 @@ return [
     'adminEmail' => 'pdekritisweb@kritis.pde.sch.gr',
     'user.passwordResetTokenExpire' => 3600,
     'user.rememberMeDuration' => 172800, // seconds of cookie duration for auto login (if enabled)
+    'director' => 'Γεώργιος Τερζάκης',
+    'surrogate' => 'Γεώργιος Περικλειδάκης',
+    'leavePerson' => 'Φ. Ματθαίου',
+    'leavePhone' => '2810 302449',
+    'leaveFax' => '2810 302447',
+    'transportPerson' => 'Α. Βασιλειάδη',
+    'transportPhone' => '2810 347290',
+    'transportFax' => '2810 301103',
 ];
