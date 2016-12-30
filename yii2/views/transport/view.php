280a68cb1b80ff2601985633e51977214848e65c
Manos Poulakis <manpoul@gmail.com>
2016-12-30 08:55:04 +0200

    transport-module update-delete buttons hide for locked transports

--- yii2/views/transport/view.php
+++ yii2/views/transport/view.php
@@ -30,16 +30,20 @@ if ($model->deleted) {
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= 
-        Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) 
-        ?>
+		<?php if ($model->locked == False) { ?>
+			<?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+			<?= 
+			Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+				'class' => 'btn btn-danger',
+				'data' => [
+					'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+					'method' => 'post',
+				],
+			]) 
+			?>
+        <?php } else { 
+					echo " <h2><small>Αυτή η μετακίνηση είναι κλειδωμένη επειδή έχει χρησιμοποιηθεί σε αποστολή πληρωμής εξόδων κίνησης.</small></h2>  ";
+				} ?>
         <?=
         Html::a(Yii::t('app', 'Transport file'), ['print', 'id' => $model->id, 'ftype' => Transport::fapproval ], [
             'class' => 'btn btn-warning',
