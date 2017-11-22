2bc97268e0870eeb52dc2e6bc3112cf35301adcf
Stavros Papadakis <spapad@gmail.com>
2017-11-22 12:25:37 +0200

    Add prefectures migration

---
+++ yii2/modules/SubstituteTeacher/migrations/m171122_101454_prefectures.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+class m171122_101454_prefectures extends Migration
+{
+
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        Yii::$app->db->createCommand("SET SQL_MODE='ALLOW_INVALID_DATES'")->execute();
+
+        $this->createTable('{{%stprefecture}}', [
+            'id' => $this->primaryKey(),
+            'region' => $this->string(150)->notNull()->defaultValue(''),
+            'prefecture' => $this->string(150)->notNull()->unique(),
+            ], $tableOptions);
+
+        $this->batchInsert('{{%stprefecture}}', ['region', 'prefecture'], [
+            ['ΚΡΗΤΗΣ', 'ΗΡΑΚΛΕΙΟΥ'],
+            ['ΚΡΗΤΗΣ', 'ΛΑΣΙΘΙΟΥ'],
+            ['ΚΡΗΤΗΣ', 'ΡΕΘΥΜΝΟΥ'],
+            ['ΚΡΗΤΗΣ', 'ΧΑΝΙΩΝ'],
+        ]);
+
+        $this->addColumn('{{%stposition}}', 'prefecture_id', $this->integer()->after('specialisation_id'));
+        $this->addForeignKey('fk_prefecture_id', '{{%stposition}}', 'prefecture_id', '{{%stprefecture}}', 'id', 'RESTRICT', 'CASCADE');
+    }
+
+    public function safeDown()
+    {
+        Yii::$app->db->createCommand("SET SQL_MODE='ALLOW_INVALID_DATES'")->execute();
+
+        $this->dropForeignKey('fk_prefecture_id', '{{%stposition}}');
+        $this->dropColumn('{{%stposition}}', 'prefecture_id');
+
+        $this->dropTable('{{%stprefecture}}');
+    }
+}
