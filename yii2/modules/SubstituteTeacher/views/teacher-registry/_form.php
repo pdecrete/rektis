db07814822d7a7bca24128e7f0e9a59bb58a9435
Stavros Papadakis <spapad@gmail.com>
2018-03-28 14:06:51 +0300

    Add qualification info to teacher registry

--- yii2/modules/SubstituteTeacher/views/teacher-registry/_form.php
+++ yii2/modules/SubstituteTeacher/views/teacher-registry/_form.php
@@ -6,19 +6,20 @@ use kartik\select2\Select2;
 use app\modules\SubstituteTeacher\models\TeacherRegistry;
 use app\modules\SubstituteTeacher\models\Specialisation;
 use kartik\datecontrol\DateControl;
+use dosamigos\switchinput\SwitchBox;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\SubstituteTeacher\models\TeacherRegistry */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-    <div class="teacher-registry-form">
+<div class="teacher-registry-form">
 
-        <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(); ?>
 
-        <div class="row">
-            <div class="col-md-6">
-                <?=
+    <div class="row">
+        <div class="col-md-6">
+            <?=
                 $form->field($model, 'specialisation_ids')->widget(Select2::classname(), [
                     'data' => Specialisation::selectables(),
                     'options' => ['placeholder' => Yii::t('substituteteacher', 'Choose...')],
@@ -29,113 +30,191 @@ use kartik\datecontrol\DateControl;
                 ]);
 
                 ?>
-            </div>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-md-6">
-                <?= $form->field($model, 'surname')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-md-6">
-                <?= $form->field($model, 'firstname')->textInput(['maxlength' => true]) ?>
-            </div>
+    <div class="row">
+        <div class="col-md-6">
+            <?= $form->field($model, 'surname')->textInput(['maxlength' => true]) ?>
         </div>
-
-        <div class="row">
-            <div class="col-md-6">
-                <?= $form->field($model, 'fathername')->textInput(['maxlength' => true]) ?>
-
-            </div>
-            <div class="col-md-6">
-                <?= $form->field($model, 'mothername')->textInput(['maxlength' => true]) ?>
-            </div>
+        <div class="col-md-6">
+            <?= $form->field($model, 'firstname')->textInput(['maxlength' => true]) ?>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-md-4">
-                <?= $form->field($model, 'gender')->dropDownList(TeacherRegistry::getChoices('gender'), ['prompt' => Yii::t('substituteteacher', 'Choose...')]) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'marital_status')->dropDownList(TeacherRegistry::getChoices('marital_status'), ['prompt' => Yii::t('substituteteacher', 'Choose...')]) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'protected_children')->textInput(['type' => 'number']) ?>
-            </div>
+    <div class="row">
+        <div class="col-md-6">
+            <?= $form->field($model, 'fathername')->textInput(['maxlength' => true]) ?>
+
         </div>
+        <div class="col-md-6">
+            <?= $form->field($model, 'mothername')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
 
-        <div class="row">
-            <div class="col-md-4">
-                <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'birthdate')->widget(DateControl::classname(), [
+    <div class="row">
+        <div class="col-md-4">
+            <?= $form->field($model, 'gender')->dropDownList(TeacherRegistry::getChoices('gender'), ['prompt' => Yii::t('substituteteacher', 'Choose...')]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'marital_status')->dropDownList(TeacherRegistry::getChoices('marital_status'), ['prompt' => Yii::t('substituteteacher', 'Choose...')]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'protected_children')->textInput(['type' => 'number']) ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'birthdate')->widget(DateControl::classname(), [
                     'type'=>DateControl::FORMAT_DATE
+                ]);
+                ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'birthplace')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <?= $form->field($model, 'mobile_phone')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'home_phone')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'work_phone')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <?= $form->field($model, 'home_address')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'postal_code')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <?= $form->field($model, 'social_security_number')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'tax_identification_number')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'tax_service')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <?= $form->field($model, 'identity_number')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'iban')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-md-4">
+            <?= $form->field($model, 'bank')->textInput(['maxlength' => true]) ?>
+        </div>
+    </div>
+
+    <div class="well well-sm">
+        <h3>Τυπικά προσόντα</h3>
+        <div class="row">
+            <div class="col-md-4">
+                <?= $form->field($model, 'aei')->widget(SwitchBox::className(), [
+                    'options' => [
+                        'label' => '',
+                    ],
+                    'clientOptions' => [
+                       'size' => 'small',
+                        'onColor' => 'success',
+                        'onText' => Yii::t('substituteteacher', 'YES'),
+                        'offText' => Yii::t('substituteteacher', 'No'),
+                    ]
                 ]);
                 ?>
             </div>
             <div class="col-md-4">
-                <?= $form->field($model, 'birthplace')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'tei')->widget(SwitchBox::className(), [
+                    'options' => [
+                        'label' => '',
+                    ],
+                    'clientOptions' => [
+                       'size' => 'small',
+                        'onColor' => 'success',
+                        'onText' => Yii::t('substituteteacher', 'YES'),
+                        'offText' => Yii::t('substituteteacher', 'No'),
+                    ]
+                ]);
+                ?>
+            </div>
+            <div class="col-md-4">
+                <?= $form->field($model, 'epal')->widget(SwitchBox::className(), [
+                    'options' => [
+                        'label' => '',
+                    ],
+                    'clientOptions' => [
+                       'size' => 'small',
+                        'onColor' => 'success',
+                        'onText' => Yii::t('substituteteacher', 'YES'),
+                        'offText' => Yii::t('substituteteacher', 'No'),
+                    ]
+                ]);
+                ?>
             </div>
         </div>
-
         <div class="row">
             <div class="col-md-4">
-                <?= $form->field($model, 'mobile_phone')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'sign_language')->widget(SwitchBox::className(), [
+                    'options' => [
+                        'label' => '',
+                    ],
+                    'clientOptions' => [
+                       'size' => 'small',
+                        'onColor' => 'success',
+                        'onText' => Yii::t('substituteteacher', 'YES'),
+                        'offText' => Yii::t('substituteteacher', 'No'),
+                    ]
+                ]);
+                ?>
             </div>
             <div class="col-md-4">
-                <?= $form->field($model, 'home_phone')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'work_phone')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'braille')->widget(SwitchBox::className(), [
+                    'options' => [
+                        'label' => '',
+                    ],
+                    'clientOptions' => [
+                       'size' => 'small',
+                        'onColor' => 'success',
+                        'onText' => Yii::t('substituteteacher', 'YES'),
+                        'offText' => Yii::t('substituteteacher', 'No'),
+                    ]
+                ]);
+                ?>
             </div>
         </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <?= $form->field($model, 'home_address')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'postal_code')->textInput(['maxlength' => true]) ?>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <?= $form->field($model, 'social_security_number')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'tax_identification_number')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'tax_service')->textInput(['maxlength' => true]) ?>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <?= $form->field($model, 'identity_number')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'iban')->textInput(['maxlength' => true]) ?>
-            </div>
-            <div class="col-md-4">
-                <?= $form->field($model, 'bank')->textInput(['maxlength' => true]) ?>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'comments')->textarea(['rows' => 6]) ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <?= Html::submitButton($model->isNewRecord ? Yii::t('substituteteacher', 'Create') : Yii::t('substituteteacher', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-        </div>
-
-        <?php ActiveForm::end(); ?>
-
     </div>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <?= $form->field($model, 'comments')->textarea(['rows' => 6]) ?>
+        </div>
+    </div>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('substituteteacher', 'Create') : Yii::t('substituteteacher', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
--- yii2/modules/SubstituteTeacher/views/teacher-registry/view.php
+++ yii2/modules/SubstituteTeacher/views/teacher-registry/view.php
@@ -13,110 +13,146 @@ $this->title = $model->name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('substituteteacher', 'Teacher Registries'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-    <div class="teacher-registry-view">
+<div class="teacher-registry-view">
 
-        <h1>
-            <?= Html::encode($this->title) ?>
-        </h1>
+    <h1>
+        <?= Html::encode($this->title) ?>
+    </h1>
 
-        <p>
-            <?= Html::a(Yii::t('substituteteacher', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-                <?= Html::a(Yii::t('substituteteacher', 'Delete'), ['delete', 'id' => $model->id], [
+    <p>
+        <?= Html::a(Yii::t('substituteteacher', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a(Yii::t('substituteteacher', 'Delete'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => Yii::t('substituteteacher', 'Are you sure you want to delete this item?'),
                 'method' => 'post',
             ],
         ]) ?>
-        </p>
+    </p>
 
-        <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            [
-                'attribute' => 'specialisation_ids',
-                'value' => function ($m) {
-                    return implode(', ', $m->specialisation_labels);
-                }
-            ],
-            'gender_label',
-            'surname',
-            'firstname',
-            'fathername',
-            'mothername',
-            'marital_status_label',
-            'protected_children',
-            'mobile_phone',
-            'home_phone',
-            'work_phone',
-            'home_address',
-            'city',
-            'postal_code',
-            'social_security_number',
-            'tax_identification_number',
-            'tax_service',
-            'identity_number',
-            'bank',
-            'iban',
-            'email:email',
-            'birthdate',
-            'birthplace',
-            'comments:ntext',
-            'created_at',
-            'updated_at',
-        ],
-    ]) ?>
+    <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#personal" aria-controls="personal" role="tab" data-toggle="tab"><?= Yii::t('substituteteacher', 'Personal information') ?></a></li>
+        <li role="presentation"><a href="#financial" aria-controls="financial" role="tab" data-toggle="tab"><?= Yii::t('substituteteacher', 'Financial information') ?></a></li>
+        <li role="presentation"><a href="#qualification" aria-controls="qualification" role="tab" data-toggle="tab"><?= Yii::t('substituteteacher', 'Qualifications') ?></a></li>
+        <li role="presentation"><a href="#teacher-info" aria-controls="teacher-info" role="tab" data-toggle="tab"><?= Yii::t('substituteteacher', 'Appearances') ?></a></li>
+    </ul>
 
+    <div class="tab-content">
+        <div role="tabpanel" class="tab-pane fade-in active" id="personal">
+            <h1><?= Yii::t('substituteteacher', 'Personal information') ?></h1>
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    // 'id',
+                    [
+                        'attribute' => 'specialisation_ids',
+                        'value' => function ($m) {
+                            return implode(', ', $m->specialisation_labels);
+                        }
+                    ],
+                    'gender_label',
+                    'surname',
+                    'firstname',
+                    'fathername',
+                    'mothername',
+                    'marital_status_label',
+                    'protected_children',
+                    'mobile_phone',
+                    'home_phone',
+                    'work_phone',
+                    'home_address',
+                    'city',
+                    'postal_code',
+                    'social_security_number',
+                    'identity_number',
+                    'email:email',
+                    'birthdate',
+                    'birthplace',
+                    'comments:ntext',
+                    'created_at',
+                    'updated_at',
+                ],
+            ]) ?>
+        </div>
+        <div role="tabpanel" class="tab-pane fade-in" id="financial">
+            <h1><?= Yii::t('substituteteacher', 'Financial information') ?></h1>
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'tax_identification_number',
+                    'tax_service',
+                    'bank',
+                    'iban',
+                ],
+            ]) ?>
+        </div>
+        <div role="tabpanel" class="tab-pane fade-in" id="qualification">
+            <h1><?= Yii::t('substituteteacher', 'Qualifications') ?></h1>
+            <?= DetailView::widget([
+                'model' => $model,
+                'attributes' => [
+                    'sign_language:boolean',
+                    'braille:boolean',
+                    'epal:boolean',
+                    'tei:boolean',
+                    'aei:boolean',
+                ],
+            ]) ?>
+        </div>
+        <div role="tabpanel" class="tab-pane fade-in" id="teacher-info">
+            <h1><?= Yii::t('substituteteacher', 'Appearances') ?></h1>
+            <?php if (empty($model->teachers)) : ?>
+            <p class="text-info"><?= Yii::t('substituteteacher', 'No information') ?></p>
+            <?php else : ?>
+            <?php 
+            $dataProvider2 = new ArrayDataProvider([
+                'allModels' => $model->getTeachers()->orderBy(['year' => SORT_DESC])->all(),
+            ]);
+            ?>
+            <div class="teacher-index">
+                <?= GridView::widget([
+                    'dataProvider' => $dataProvider2,
+                    'filterModel' => null,
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+
+                        'id',
+                        'year',
+                        [
+                            'attribute' => 'status',
+                            'value' => 'status_label',
+                        ],
+                        [
+                            'attribute' => '',
+                            'header' => Yii::t('substituteteacher', 'Teacher boards'),
+                            'value' => function ($m) {
+                                return $m->boards ? implode(
+                                    '<br>',
+                                    array_map(function ($model) {
+                                        return $model->label;
+                                    }, $m->boards)
+                                ) : null;
+                            },
+                            'format' => 'html'
+                        ],
+                        [
+                            'attribute' => '',
+                            'header' => Yii::t('substituteteacher', 'Placement preferences'),
+                            'value' => function ($m) {
+                                return $m->placementPreferences ? implode(
+                                    '<br>',
+                                    array_map(function ($pref) {
+                                        return $pref->label_for_teacher;
+                                    }, $m->placementPreferences)
+                                ) : null;
+                            },
+                            'format' => 'html'
+                        ],
+                    ],
+                ]); ?>
+            </div>
+            <?php endif; ?>
+        </div>
     </div>
+</div>
 
-    <?php if (!empty($model->teachers)) : ?>
-    <?php 
-    $dataProvider2 = new ArrayDataProvider([
-        'allModels' => $model->getTeachers()->orderBy(['year' => SORT_DESC])->all(),
-    ]);
-    ?>
-    <div class="teacher-index">
-        <h1><?= Yii::t('substituteteacher', 'Appearances') ?></h1>
-        <?= GridView::widget([
-        'dataProvider' => $dataProvider2,
-        'filterModel' => null,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'year',
-            [
-                'attribute' => 'status',
-                'value' => 'status_label',
-            ],
-            [
-                'attribute' => '',
-                'header' => Yii::t('substituteteacher', 'Teacher boards'),
-                'value' => function ($m) {
-                    return $m->boards ? implode(
-                        '<br>',
-                        array_map(function ($model) {
-                            return $model->label;
-                        }, $m->boards)
-                    ) : null;
-                },
-                'format' => 'html'
-            ],
-            [
-                'attribute' => '',
-                'header' => Yii::t('substituteteacher', 'Placement preferences'),
-                'value' => function ($m) {
-                    return $m->placementPreferences ? implode(
-                        '<br>',
-                        array_map(function ($pref) {
-                            return $pref->label_for_teacher;
-                        }, $m->placementPreferences)
-                    ) : null;
-                },
-                'format' => 'html'
-            ],
-        ],
-    ]); ?>
-    </div>
-    <?php endif; ?>
