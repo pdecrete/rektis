6c70013e962e95e7beb2d1295039d1f69cf65eff
Stavros Papadakis <spapad@gmail.com>
2018-04-27 13:39:03 +0300

    Update main.php

--- yii2/views/layouts/main.php
+++ yii2/views/layouts/main.php
@@ -68,6 +68,17 @@ AppAsset::register($this);
                         ],
 
                         '<li class="divider"></li>',
+                        '<li class="dropdown-header"><i class="glyphicon glyphicon-pencil"></i> Σελίδες</li>',
+                        [
+                            'label' => 'Όλες οι σελίδες',
+                            'url' => ['/Pages/page/index']
+                        ],
+                        [
+                            'label' => 'Νέα σελίδα',
+                            'url' => ['/Pages/page/create']
+                        ],
+
+                        '<li class="divider"></li>',
                         '<li class="dropdown-header"><i class="glyphicon glyphicon-dashboard"></i> Παράμετροι</li>',
                         [
                             'label' => 'Auth items',
