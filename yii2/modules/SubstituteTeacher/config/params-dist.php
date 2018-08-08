e2b3507071b04e0cedd9a133d23fd181d4ce9a87
Stavros Papadakis <spapad@gmail.com>
2018-08-08 10:07:05 +0300

    Add specialisations; Add clear command for tests

--- yii2/modules/SubstituteTeacher/config/params-dist.php
+++ yii2/modules/SubstituteTeacher/config/params-dist.php
@@ -10,10 +10,16 @@ return [
         'crypt-key-file' => "/path/to/your/key.file",
         // "codes" of specialisations this module *handles*; used to filter specialisations
         'applicable-specialisation-codes' => [
-            'ΠΕ 2300',
-            'ΠΕ 2500',
-            'ΠΕ 3000',
-            'ΕΒΠ'
+            'ΠΕ 2126', // ΘΕΡΑΠΕΥΤΩΝ ΛΟΓΟΥ 
+            'ΠΕ 2200', // ΕΠΑΓΓΕΜΑΤΙΚΩΝ ΣΥΜΒΟΥΛΩΝ 
+            'ΠΕ 2300', // ΨΥΧΟΛΟΓΩΝ
+            'ΠΕ 2400', // ΠΑΙΔΟΨΥΧΙΑΤΡΩΝ 
+            'ΠΕ 2500', // ΣΧΟΛΙΚΩΝ ΝΟΣΗΛΕΥΤΩΝ 
+            'ΠΕ 2800', // ΦΥΣΙΚΟΘΕΡΑΠΕΥΤΩΝ 
+            'ΠΕ 2900', // ΕΡΓΑΣΙΟΘΕΡΑΠΕΥΤΩΝ 
+            'ΠΕ 3000', // ΚΟΙΝΩΝΙΚΩΝ ΛΕΙΤΟΥΡΓΩΝ 
+            'ΠΕ 3100', // ΕΞΕΙΔΙΚΕΥΜΕΝΟΥΣ
+            'ΔΕ1' // ΕΒΠ 
         ],
         'extra-call-teachers-percent' => 0.2, // call an extra 20% of teachers to cover loses; ONLY USED WHEN call does not explicitely define number of teachers to call 
     ]
