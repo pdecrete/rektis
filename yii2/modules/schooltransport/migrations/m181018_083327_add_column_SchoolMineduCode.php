8d98066746b45be403803cab352264057dc1e2af
Giannis Chaniotakis <jhaniot@hotmail.com>
2018-10-18 13:22:13 +0300

    Minedu code unique constraint

--- yii2/modules/schooltransport/migrations/m181018_083327_add_column_SchoolMineduCode.php
+++ yii2/modules/schooltransport/migrations/m181018_083327_add_column_SchoolMineduCode.php
@@ -9,9 +9,11 @@ class m181018_083327_add_column_SchoolMineduCode extends Migration
     {
         $altered_table = $this->db->tablePrefix . 'schoolunit';
         $alter_command = "ALTER TABLE " . $altered_table . " ADD COLUMN `school_mineducode` VARCHAR(50) COMMENT 'Κωδικός σχολείου από ΥΠΠΕΘ';"; /*VARCHAR BECAUSE THERE ARE I.E. SEK026 CODES*/
+        $alter_columnunique_command =  "ALTER TABLE " . $altered_table . " ADD CONSTRAINT school_mineducode UNIQUE (school_mineducode)";
         Console::stdout("\n*** Adding new column (school_mineducode) in table " . $altered_table . ". *** \n");
         Console::stdout("SQL Command: " . $alter_command . "\n");
         Yii::$app->db->createCommand($alter_command)->execute();
+        Yii::$app->db->createCommand($alter_columnunique_command)->execute();        
     }
     
     public function safeDown()
